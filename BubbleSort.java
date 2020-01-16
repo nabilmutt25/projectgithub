9c7a3330c69d127c252e0f5e45fc8423a92e0096
nikita <nikita>
2020-01-16 14:02:54 +0700

    a

--- BubbleSort.java
+++ BubbleSort.java
@@ -27,7 +27,7 @@ class BubbleSort{
     public static void main(String args[])
     {
         BubbleSort ob = new BubbleSort();
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        int arr[] = {60, 34, 25, 12, 22, 11, 90};
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
